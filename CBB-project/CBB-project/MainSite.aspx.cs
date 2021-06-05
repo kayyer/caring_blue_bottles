e70bd9002653be27c8ea2474b10d2e5444a96d5f
Ziaja Bálint <55186654+ZiajaB@users.noreply.github.com>
2021-06-05 20:54:35 +0200

    SQL létrehozása + SQL elérése SiteMaster-ből

--- CBB-project/CBB-project/MainSite.aspx.cs
+++ CBB-project/CBB-project/MainSite.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CBB_project.Classes;
 
 namespace CBB_project
 {
--- CBB-project/CBB-project/Site.Master.cs
+++ CBB-project/CBB-project/Site.Master.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CBB_project.Classes;
+
 
 namespace CBB_project
 {
     public partial class SiteMaster : MasterPage
     {
+        /// <summary>
+        /// SQL eléréséhez szükséges publikus interfész
+        /// </summary>
+        public SQLIF SqlIF;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            SqlIF = new SQLIF(System.Configuration.ConfigurationManager.ConnectionStrings["CBBDB"].ConnectionString);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
